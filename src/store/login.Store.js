364604af6723a8c7b93e48a6663940222df462c1
lipstickof87 <2380588373@qq.com>
2023-10-17 16:49:31 +0800

    4.49

--- src/store/login.Store.js
+++ src/store/login.Store.js
@@ -1,3 +1,16 @@
 import { makeAutoObservable } from "mobx"
 import { http } from '@/utils'
 
+class LoginStore {
+  token = ''
+  constructor() {
+    makeAutoObservable(this)
+  }
+  //login
+  login = async ({ mobile, code }) => {
+    const res = await http.post('http://geek.itheima.net/v1_0/authorizations', {
+      mobile,code
+    })
+    console.log(res)
+  }
+}export default LoginStore
